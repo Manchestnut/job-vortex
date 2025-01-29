1d287ed749ff9de0c1dacdbc13e2996026015d3c
Luke <luke.manongsong@gmail.com>
2025-01-29 13:01:58 +0800

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import Image from "next/image";
-import { FaArrowRightLong } from "react-icons/fa6";
-import { AiOutlineMenu } from "react-icons/ai";
 import { MdOutlineEngineering } from "react-icons/md";
 import { BiHealth } from "react-icons/bi";
 import { MdOutlineRoomService } from "react-icons/md";
