47face9d06f7d65148b4215e9f4b6f6c3f91ec43
Luke <luke.manongsong@gmail.com>
2025-01-29 13:54:38 +0800

    fix

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -7,8 +7,11 @@ import { FaArrowRightLong } from "react-icons/fa6";
 export default function NavBar() {
     return(
         <nav className="bg-blue-100 p-3 flex justify-between items-center py-6">
-          <Link href={'/'} rel="noopener noreferrer"><Image src="/logo.png" alt="logo" width={150} height={100}></Image></Link>  
-
+          <Link href={'/'} rel="noopener noreferrer">
+            <div className="w-[150px] h-[50px] relative">
+                <Image src="/logo.png" alt="logo" width={150} height={50} priority className="object-contain"></Image>
+            </div>
+          </Link>  
           <AiOutlineMenu className="text-3xl fill-blue-900 lg:hidden"/>
           <div className="lg:flex gap-6 px-4 hidden">
             <a><button className="bg-[#F1F1F1] px-3 py-1 rounded-xl flex items-center gap-2 text-xl">Apply <FaArrowRightLong /></button></a>
