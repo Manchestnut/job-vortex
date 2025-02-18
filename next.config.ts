811e4effd6c27a59ad5640c8cda2fc7b23ad3cfe
Luke <luke.manongsong@gmail.com>
2025-02-18 15:47:18 +0800

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-},
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://www.jobvortex.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
